ade22c2c40dead0cdda4692d0c1970e74e46631f
Daniel Rojo <danielgowrojo@gmail.com>
2024-06-02 22:29:09 -0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { Hero } from './components/Hero'
+
+function App() {
+
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
+export default App
